97c97e4f421d252ed0f415766beb1273c8d90105
Arthur Rolemberg <arthurrolemberg4262@gmail.com>
2024-01-16 14:50:34 -0300

    Trata algumas exceções deixando-as idiomáticas

--- src/main/kotlin/me/dio/credit/application/system/dto/CreditDto.kt
+++ src/main/kotlin/me/dio/credit/application/system/dto/CreditDto.kt
@@ -1,15 +1,17 @@
 package me.dio.credit.application.system.dto
 
+import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.NotNull
 import me.dio.credit.application.system.entity.Credit
 import me.dio.credit.application.system.entity.Customer
 import java.math.BigDecimal
 import java.time.LocalDate
 
 data class CreditDto(
-  val creditValue: BigDecimal,
-  val dayFirstInstallment: LocalDate,
+  @field:NotNull(message = "Invalid credit value") val creditValue: BigDecimal,
+  @field:Future val dayFirstInstallment: LocalDate,
   val numberOfInstallments: Int,
-  val customerId: Long
+  @field:NotNull(message = "Invalid customer ID") val customerId: Long
 ) {
 
   fun toEntity(): Credit = Credit(
--- src/main/kotlin/me/dio/credit/application/system/dto/CustomerDto.kt
+++ src/main/kotlin/me/dio/credit/application/system/dto/CustomerDto.kt
@@ -1,18 +1,26 @@
 package me.dio.credit.application.system.dto
 
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 import me.dio.credit.application.system.entity.Address
 import me.dio.credit.application.system.entity.Customer
+import org.hibernate.validator.constraints.br.CPF
 import java.math.BigDecimal
 
 data class CustomerDto(
-  val firstName: String,
-  val lastName: String,
+  @field:NotEmpty(message = "Invalid first name") val firstName: String,
+  @field:NotEmpty(message = "Invalid last name") val lastName: String,
+  @field:NotEmpty(message = "Invalid CPF")
+  @field:CPF(message = "This CPF is invalid")
   val cpf: String,
-  val income: BigDecimal,
+  @field:NotNull(message = "Invalid income") val income: BigDecimal,
+  @field:NotEmpty(message = "Invalid email")
+  @field:Email(message = "Invalid email")
   val email: String,
-  val password: String,
-  val zipCode: String,
-  val street: String
+  @field:NotEmpty(message = "Invalid password") val password: String,
+  @field:NotEmpty(message = "Invalid zip code") val zipCode: String,
+  @field:NotEmpty(message = "Invalid street") val street: String
 ) {
 
   fun toEntity(): Customer = Customer(
